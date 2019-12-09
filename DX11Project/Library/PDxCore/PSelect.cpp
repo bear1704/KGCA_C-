4e54f08421c0d0b4efc6b994da911307ad9d732c
soochanPark <34018251+bear1704@users.noreply.github.com>
2019-12-09 21:39:42 +0900

    피킹, OBB-Ray 충돌 제작
    피킹과 Obb-ray 충돌을 감지, 교점을 찾는 함수를 작성하였다.
    시간관계상 아직 테스트를 해보지 않았음
    내일 작업임

--- DX11Project/Library/PDxCore/PSelect.cpp
+++ DX11Project/Library/PDxCore/PSelect.cpp
@@ -68,3 +68,140 @@ bool PSelect::IsPointInTriPolygon(D3DXVECTOR3& point, D3DXVECTOR3& face_normal, 
 
 	return true;
 }
+
+void PSelect::SetMatrix(D3DXMATRIX* world, D3DXMATRIX* view, D3DXMATRIX* proj)
+{
+	if (world != nullptr)
+	{
+		mat_world_ = *world;
+
+	}
+	if (view != nullptr)
+	{
+		mat_view_ = *view;
+	}
+	if (proj != nullptr)
+	{
+		mat_proj_ = *proj;
+	}
+
+	RefreshPickingRay();
+}
+
+void PSelect::RefreshPickingRay()
+{
+	if (g_InputActionMap.leftClick == KEYSTAT::KEY_PUSH || g_InputActionMap.leftClick == KEYSTAT::KEY_HOLD)
+	{
+		POINT cursor;
+		GetCursorPos(&cursor);
+		ScreenToClient(g_hWnd, &cursor);
+
+		D3DXVECTOR3 view_coord_point;
+		D3DXVECTOR3& v = view_coord_point;
+
+		//스크린 좌표->뷰 좌표로 변환 Tbasis 2권 p.354
+		v.x = (((2.0f * cursor.x) / g_rectangle_client.right) - 1) / mat_proj_._11;
+		v.y = -(((2.0f * cursor.y) / g_rectangle_client.bottom) - 1) / mat_proj_._22;
+		v.z = 1.0f;
+		
+		D3DXMATRIX mat_world_view = mat_world_ * mat_view_;
+		D3DXMATRIX m;
+		D3DXMatrixInverse(&m, NULL, &mat_world_view);
+
+		picking_ray_.origin = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		picking_ray_.direction = D3DXVECTOR3(v.x, v.y, v.z);
+		D3DXVec3TransformCoord(&picking_ray_.origin, &picking_ray_.origin, &m);
+		D3DXVec3TransformNormal(&picking_ray_.direction, &picking_ray_.direction, &m);
+		D3DXVec3Normalize(&picking_ray_.direction, &picking_ray_.direction);
+	}
+}
+
+bool PSelect::CheckRaytoOBBCollision(P_BOX* box, PickingRay* ray)
+{
+	if (ray == nullptr) ray = &picking_ray_;
+
+
+	//s -> OBBcenter와 ray의 origin을 잇는 벡터와 분리축의 투영 결과  sa -> s의 절대값(Vec R->)
+ 	//f -> ray와 ray-box투영벡터 방향검사(p.376)를 위한 값(vec x dot vec d)   fa -> f의 절대값
+	float f[3], s[3], fa[3], sa[3];
+	float t_min = -999999.0f;
+	float t_max = 999999.0f;
+
+	D3DXVECTOR3 box_to_rayorigin = ray->origin - box->center;
+
+	//v = axis x,y,z
+	for (int v = 0; v < 3; v++)
+	{
+		f[v] = D3DXVec3Dot(&box->obb_axis[v], &ray->direction);
+		s[v] = D3DXVec3Dot(&box->obb_axis[v], &box_to_rayorigin);
+		fa[v] = std::fabs(f[v]);
+		sa[v] = std::fabs(s[v]);
+
+		//p.376 반직선 방향검사 / p.375 분리축 투영 거리 검사 
+		if (sa[v] * f[v] >= 0.0f && sa[v] > box->obb_extents[v])
+			return false;
+
+		//AABB 체크
+		float t1 = (-s[v] - box->obb_extents[v]) / f[v];
+		float t2 = (-s[v] + box->obb_extents[v]) / f[v];
+
+		if (t1 > t2)
+			swap(t1, t2);
+
+		t_min = max(t_min, t1);
+		t_max = min(t_max, t2);
+
+		if (t_min > t_max)
+		{
+			assert(false); //aabb 요소가 안들어가있을수도 있음
+			return false;
+		}
+			
+		
+	}
+
+	//fw = p.375의 OBB투영(f) fx, fy, fz
+		//sn_cross = p/376 분리축에대한 vec R = vec O - vec C 투영 sx, sy, sz
+	float sn_cross[3], fw;
+	D3DXVECTOR3 d_cross_R;
+
+	//d_cross_R = rayorigin-boxcenter 투영한 값과, ray_direction간의 외적. 즉 vec d cross vec R(p 375 하단)
+	D3DXVec3Cross(&d_cross_R, &ray->direction, &box_to_rayorigin);
+
+	//분리축 d cross axis x(vec w)
+	sn_cross[0] = fabs(D3DXVec3Dot(&d_cross_R, &box->obb_axis[0])); //Sx = abs(d cross x dot R)
+	float fx_1 = box->obb_extents[1] * fa[2];//d dot z * e[1]
+	float fx_2 = box->obb_extents[2] * fa[1];//d dot y * e[2]
+	fw = fx_1 + fx_2;
+
+	if (sn_cross[0] > fw)
+	{
+		//m_vDxR = vDxR;
+		return false;
+	}
+	//분리축 d cross axis y(vec w)
+	sn_cross[1] = fabs(D3DXVec3Dot(&d_cross_R, &box->obb_axis[1])); //Sy = abs(d cross y dot R)
+	float fy_1 = box->obb_extents[0] * fa[2]; //d dot z * e[0]
+	float fy_2 = box->obb_extents[2] * fa[0]; //d dot x * e[2]
+	fw = fy_1 + fy_2;
+
+	if (sn_cross[1] > fw)
+	{
+		return false;
+	}
+
+	//분리축 d cross axis z(vec w)
+	sn_cross[2] = fabs(D3DXVec3Dot(&d_cross_R, &box->obb_axis[2])); //Sy = abs(d cross y dot R)
+	float fz_1 = box->obb_extents[0] * fa[1]; //d dot z * e[0]
+	float fz_2 = box->obb_extents[1] * fa[0]; //d dot x * e[2]
+	fw = fz_1 + fz_2;
+
+	if (sn_cross[2] > fw)
+	{
+		return false;
+	}
+
+	intersection_ = ray->origin + ray->direction * t_min;
+	return true;
+
+}
--- DX11Project/include/PDxCore/PSelect.h
+++ DX11Project/include/PDxCore/PSelect.h
@@ -3,7 +3,14 @@
 
 struct PickingRay
 {
+	float extent;
+	D3DXVECTOR3 origin;
+	D3DXVECTOR3 direction;
 
+	PickingRay()
+	{
+		extent = -1;
+	}
 
 };
 
@@ -14,7 +21,12 @@ public:
 	PSelect();
 	~PSelect();
 
-
+public:
+	PickingRay picking_ray_;
+	D3DXMATRIX mat_world_;
+	D3DXMATRIX mat_view_;
+	D3DXMATRIX mat_proj_;
+	D3DXVECTOR3 intersection_; //레이의 원점 -> 교점까지의 벡터(정규화안됨)
 public:
 	//반직선과 평면의 교차여부를 판정하고 반직선의 교점 좌표값을 얻는다
 	bool GetIntersection(OUT_ D3DXVECTOR3& intersection, 
@@ -31,5 +43,11 @@ public:
 		D3DXVECTOR3& v0,
 		D3DXVECTOR3& v1, 
 		D3DXVECTOR3& v2);
+
+	void SetMatrix(D3DXMATRIX* world, D3DXMATRIX* view, D3DXMATRIX* proj);
+	void RefreshPickingRay();
+	//tbasis2 p.374
+	bool CheckRaytoOBBCollision(P_BOX* box, PickingRay* ray = nullptr);
+
 };
 
